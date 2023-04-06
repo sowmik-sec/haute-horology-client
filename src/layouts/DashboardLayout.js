305f384cce9fe7f8afae6bb5705b33aa5c22feac
sowmik-sec <sowmik.sec@gmail.com>
2023-04-06 18:08:19 +0600

    useSeller hook created and layout modified

--- src/layouts/DashboardLayout.js
+++ src/layouts/DashboardLayout.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import Navbar from "../shared/Navbar/Navbar";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import { AuthContext } from "../context/AuthProvider";
+import useAdmin from "../hooks/useAdmin";
+import useSeller from "../hooks/useSeller";
 
 const DashboardLayout = () => {
+  const { user } = useContext(AuthContext);
+  const [isAdmin, isAdminLoading] = useAdmin(user?.email);
+  const [isSeller, isSellerLoading] = useSeller(user?.email);
   return (
     <div>
       <Navbar />
@@ -18,12 +24,32 @@ const DashboardLayout = () => {
         <div className="drawer-side">
           <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 bg-base-100 text-base-content">
-            <li>
-              <a>Sidebar Item 1</a>
-            </li>
-            <li>
-              <a>Sidebar Item 2</a>
-            </li>
+            {isSeller && (
+              <>
+                <li>
+                  <Link>My Products</Link>
+                </li>
+                <li>
+                  <Link>Add Product</Link>
+                </li>
+                <li>
+                  <Link>My buyers</Link>
+                </li>
+              </>
+            )}
+            {isAdmin && (
+              <>
+                <li>
+                  <Link>All Sellers</Link>
+                </li>
+                <li>
+                  <Link>All Buyers</Link>
+                </li>
+                <li>
+                  <Link>Reported Items</Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
