0b770e3239ae7efecdf06c074934522ad061dc23
sowmik-sec <sowmik.sec@gmail.com>
2023-04-08 11:52:28 +0600

    my watches table created for seller

--- src/pages/Dashboard/seller/MyWatches/MyWatches.js
+++ src/pages/Dashboard/seller/MyWatches/MyWatches.js
@@ -14,7 +14,53 @@ const MyWatches = () => {
       .then((data) => setWatches(data));
   }, [user.email]);
   console.log(watches);
-  return <div></div>;
+  return (
+    <div className="overflow-x-auto mt-6">
+      <table className="table w-full">
+        {/* head */}
+        <thead>
+          <tr>
+            <th></th>
+            <th>Model</th>
+            <th>Price</th>
+            <th>Status</th>
+            <th>Advertise</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {watches.map((watch, i) => (
+            <tr key={watch._id} className="hover">
+              <th>{i + 1}</th>
+              <td>{watch.model}</td>
+              <td>${watch.price}</td>
+              <td>
+                {watch?.status === "sold" ? (
+                  <p className="text-green-400">Sold</p>
+                ) : (
+                  <p>Available</p>
+                )}
+              </td>
+              <td>
+                {watch?.status !== "sold" && watch?.advertise !== true ? (
+                  <button className="btn btn-xs btn-primary">Advertise</button>
+                ) : (
+                  ""
+                )}
+              </td>
+              <td>
+                {watch?.status !== "sold" ? (
+                  <button className="btn btn-xs btn-error">Delete</button>
+                ) : (
+                  ""
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default MyWatches;
