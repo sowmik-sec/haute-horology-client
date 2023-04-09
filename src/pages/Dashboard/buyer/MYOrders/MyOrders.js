ac77e71623b3c32e2dd5e85d43b18c5f9efa4896
sowmik-sec <sowmik.sec@gmail.com>
2023-04-09 17:23:23 +0600

    order list shown

--- src/pages/Dashboard/buyer/MYOrders/MyOrders.js
+++ src/pages/Dashboard/buyer/MYOrders/MyOrders.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useQuery } from "react-query";
+import { AuthContext } from "../../../../context/AuthProvider";
+import LoaderSpinner from "../../../../shared/Navbar/LoaderSpinner/LoaderSpinner";
 
 const MyOrders = () => {
+  const { user } = useContext(AuthContext);
+  const { isLoading, data: orders } = useQuery({
+    queryKey: ["my-orders", user?.email],
+    queryFn: () =>
+      fetch(`http://localhost:5000/my-orders?email=${user?.email}`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }).then((res) => res.json()),
+  });
+  console.log(orders);
+  if (isLoading) {
+    return <LoaderSpinner />;
+  }
   return (
     <div>
       <h1 className="text-4xl">My Orders</h1>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Model</th>
+              <th>Price</th>
+              <th>Payment</th>
+              <th>Cancel Order</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, i) => (
+              <tr key={order._id}>
+                <th>{i + 1}</th>
+                <td>{order.model}</td>
+                <td>{order.price}</td>
+                <td>
+                  {order.paymentStatus === "unpaid" ? (
+                    <button className="btn btn-primary btn-sm">Pay</button>
+                  ) : (
+                    <p className="text-green-400">Paid</p>
+                  )}
+                </td>
+                <td>
+                  {order.paymentStatus === "unpaid" ? (
+                    <button className="btn btn-error btn-sm">Delete</button>
+                  ) : (
+                    ""
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
