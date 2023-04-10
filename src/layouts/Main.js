c04bdcd42e8a2204731de87804cb119de2afa9df
sowmik-sec <sowmik.sec@gmail.com>
2023-04-10 19:39:58 +0600

    footer added

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../shared/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "../shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
