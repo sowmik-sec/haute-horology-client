d4411aad00c6c26a2943a77a7ff4563ed78a91e5
sowmik-sec <sowmik.sec@gmail.com>
2023-04-10 15:17:13 +0600

    all sellers shown in dashboard

--- src/pages/Dashboard/admin/AllSellers/AllSellers.js
+++ src/pages/Dashboard/admin/AllSellers/AllSellers.js
@@ -1,9 +1,41 @@
 import React from "react";
+import { useQuery } from "react-query";
 
 const AllSellers = () => {
+  const { isLoading, data: sellers } = useQuery({
+    queryKey: ["sellers-all"],
+    queryFn: () =>
+      fetch(`http://localhost:5000/sellers-all`, {
+        headers: {
+          authorization: `bearer ${localStorage.getItem("accessToken")}`,
+        },
+      }).then((res) => res.json()),
+  });
+  console.log(sellers);
   return (
     <div>
-      <h3>All sellers here</h3>
+      <h3>All sellers</h3>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {sellers?.map((seller, i) => (
+              <tr key={seller._id} className="hover">
+                <th>{i + 1}</th>
+                <td>{seller.name}</td>
+                <td>{seller.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
